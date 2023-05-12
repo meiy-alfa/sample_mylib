8f561eb108d047251d29c4f2461af455cb166546
meiy-alfa <miftahalfaruq@gmail.com>
2023-05-12 08:12:24 +0700

    setting model and process login

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\models\StaffModel;
+
+class Auth extends BaseController
+{
+    protected $staffmodel;
+
+    public function __construct()
+    {
+        $this->staffmodel = new StaffModel();
+    }
+
+    public function login()
+    {
+        $post = $this->request->getPost();
+        $datapost = $this->staffmodel->where(['email' => $post['email']])->first();
+        if($datapost)
+        {
+            if(md5($post['password']) == $datapost['password'])
+            {
+                $key = array
+                (
+                    'id'        => $datapost['id'],
+                    'name'      => $datapost['name'],
+                    'email'     => $datapost['email'],
+                );
+                session()->set($key);
+                return redirect()->to(base_url('home'))->withInput();
+            } else {
+                return redirect()->to(base_url(''))->with('error','password tidak sesuai');                
+            }
+        } else {
+            return redirect()->to(base_url(''))->with('error','email tidak ditemukan');
+        }
+    }
+
+    public function logout()
+    {
+        session()->remove('id','name','email');
+        return redirect()->to(base_url())->with('error','sesion selesai');
+    }
+
+}
--- app/Views/sample.php
+++ app/Views/sample.php
@@ -7,7 +7,7 @@
 		<div class="">
 			<div class="page-title">
 				<div class="title_left">
-					<h3>HOme</h3>
+					<h3>Judul</h3>
 				</div>
 			</div>
 			<div class="clearfix"></div>
@@ -15,7 +15,7 @@
 				<div class="col-md-12 col-sm-12 ">
 					<div class="x_panel">
 						<div class="x_content">
-							<p>Statistik Perpustakaan mulai dari jumlah staf, jumlah buku, jumlah anggota, dan data peminjaman</p>
+							<p>Conten</p>
 						</div>
 					</div>
 				</div>
